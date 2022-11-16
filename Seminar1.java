d1a3ed6b9db524e0cd822d6447d191d30f4e3fa8
Alex QAsper <semidog@gmail.com>
2022-11-16 18:56:24 +0500

    1 task done

--- Seminar1.java
+++ Seminar1.java
@@ -1,17 +1,24 @@
+import java.io.Console;
+import java.io.IOException;
+
 // 1. Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
 
-
+/**
+ * Seminar1
+ */
 public class Seminar1 {
-    public static void main(String[] args) throws Exception {
-        int result = showTotalAmount(5);
-    }
-    
-    public static int showTotalAmount(int value) {
-        int result = 0;
-        for (int i = 1; i <= value; i++) {
-            result += i;
+
+        public static void main(String[] args) throws IOException {
+            int result = showTotalAmount(15);
+            System.out.println(result);
         }
         
-        return result;
-    }
+        public static int showTotalAmount(int value) {
+            int result = 0;
+            for (int i = 1; i <= value; i++) {
+                result += i;
+            }
+            return result;
+        }
+        
 }
